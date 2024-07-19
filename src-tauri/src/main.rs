53a0a2ee7bd9bbf70cd721a07a98018010b25dab
JoeyShapiro <joeyashapiro@gmail.com>
2024-07-19 16:58:26 -0500

    added controller thread

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::time::UNIX_EPOCH;
+
+use rocksdb::{DB, Options};
+
 use gilrs::{Gilrs, Button, Event};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -10,59 +14,44 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    use rocksdb::{DB, Options};
-    // NB: db is automatically closed at end of lifetime
-    let path = "coca-rocks.db";
-    {
+    //     match db.get(b"my key") {
+    //         Ok(Some(value)) => println!("retrieved value {}", String::from_utf8(value).unwrap()),
+    //         Ok(None) => println!("value not found"),
+    //         Err(e) => println!("operational problem encountered: {}", e),
+    //     }
+    //     db.delete(b"my key").unwrap();
+    // }
+    // let _ = DB::destroy(&Options::default(), path);
+
+    // run gilrs in a separate thread
+    let gilrs_thread = std::thread::spawn(|| {
+        let mut gilrs = Gilrs::new().unwrap();
+        let path = "coca-rocks.db";
         let db = DB::open_default(path).unwrap();
-        db.put(b"my key", b"my value").unwrap();
-        match db.get(b"my key") {
-            Ok(Some(value)) => println!("retrieved value {}", String::from_utf8(value).unwrap()),
-            Ok(None) => println!("value not found"),
-            Err(e) => println!("operational problem encountered: {}", e),
-        }
-        db.delete(b"my key").unwrap();
-    }
-    std::process::exit(0);
-    let _ = DB::destroy(&Options::default(), path);
 
-    let mut gilrs = Gilrs::new().unwrap();
-
-    // Iterate over all connected gamepads
-    for (_id, gamepad) in gilrs.gamepads() {
-        println!("{} is {:?}", gamepad.name(), gamepad.power_info());
-    }
-
-    let mut active_gamepad = None;
-
-    loop {
-        // Examine new events
-        while let Some(Event { id, event, time }) = gilrs.next_event() {
-            /* 
-                AxisChanged(
-                    RightStickY,
-                    0.03125,
-                    Code(
-                        EvCode(
-                            EvCode {
-                                page: 1,
-                                usage: 53 
-                            }
-                        )
-                    )
-                )
-            */
-            println!("{:?} New event from {}: {:?}", time, id, event);
-            active_gamepad = Some(id);
+        // Iterate over all connected gamepads
+        for (_id, gamepad) in gilrs.gamepads() {
+            println!("{} is {:?}", gamepad.name(), gamepad.power_info());
         }
 
-        // You can also use cached gamepad state
-        if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
-            if gamepad.is_pressed(Button::South) {
-                println!("Button South is pressed (XBox - A, PS - X)");
+        loop {
+            // Examine new events
+            while let Some(Event { id, event, time }) = gilrs.next_event() {
+                let serialized = serde_json::to_string(&event).unwrap();
+
+                println!("serialized = {}", serialized);
+                // convert time to unix time
+                let unix_time = time.duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis();
+                db.put(unix_time.to_ne_bytes(), serialized).unwrap();
+                
+                // check if it is a connection event
+                if event == gilrs::ev::EventType::Connected {
+                    let gamepad = gilrs.gamepad(id);
+                    println!("connected: {:?}; power: {:?}; ff: {:?}", gamepad.name(), gamepad.power_info(), gamepad.is_ff_supported());
+                }
             }
         }
-    }
+    });
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
