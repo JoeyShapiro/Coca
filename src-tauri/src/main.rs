55cce0469e0b35e690105257b92070d646c3b005
JoeyShapiro <joeyashapiro@gmail.com>
2024-07-20 20:56:53 -0500

    changed events logged

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,7 +5,7 @@ use std::time::UNIX_EPOCH;
 
 // use rocksdb::{DB, Options};
 
-use gilrs::{Gilrs, Button, Event};
+use gilrs::{Button, Event, Gamepad, Gilrs};
 use serde::Serialize;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -17,8 +17,16 @@ fn greet(name: &str) -> String {
 #[derive(Serialize)]
 struct Application {
     name: String,
+    controller: String,
     presses: i32,
     combos: i32,
+}
+
+#[derive(Serialize)]
+struct Rock {
+    at: u128,
+    pad: String,
+    event: gilrs::EventType,
 }
 
 #[tauri::command]
@@ -27,16 +35,19 @@ fn applications() -> Vec<Application> {
     // add some applications
     apps.push(Application {
         name: "Firefox".to_string(),
+        controller: "Keyboard".to_string(),
         presses: 0,
         combos: 0,
     });
     apps.push(Application {
         name: "Hatsune Miku Project Diva 2nd Stage".to_string(),
+        controller: "PS5 Controller".to_string(),
         presses: 10_000_000,
         combos: 20,
     });
     apps.push(Application {
         name: "Skyrim".to_string(),
+        controller: "Xbox One S".to_string(),
         presses: 1_000_000,
         combos: 10,
     });
@@ -56,33 +67,42 @@ fn main() {
 
     // run gilrs in a separate thread
     let gilrs_thread = std::thread::spawn(|| {
-        let mut gilrs = Gilrs::new().unwrap();
-        let path = "coca-rocks.db";
-        // let db = DB::open_default(path).unwrap();
+    let mut gilrs = Gilrs::new().unwrap();
+    let path = "coca-rocks.db";
+    // let db = DB::open_default(path).unwrap();
 
-        // Iterate over all connected gamepads
-        for (_id, gamepad) in gilrs.gamepads() {
-            println!("{} is {:?}", gamepad.name(), gamepad.power_info());
-        }
+    // Iterate over all connected gamepads
+    for (_id, gamepad) in gilrs.gamepads() {
+        println!("{} is {:?}", gamepad.name(), gamepad.power_info());
+    }
 
-        loop {
-            // Examine new events
-            while let Some(Event { id, event, time }) = gilrs.next_event() {
-                let serialized = serde_json::to_string(&event).unwrap();
+    let mut pad = "?".to_string();
 
-                println!("serialized = {}", serialized);
-                // convert time to unix time
-                let unix_time = time.duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis();
-                // db.put(unix_time.to_ne_bytes(), serialized).unwrap();
+    loop {
+        // Examine new events
+        while let Some(Event { id, event, time }) = gilrs.next_event() {
+            // check if it is a connection event
+            if event == gilrs::ev::EventType::Connected {
+                let gamepad = gilrs.gamepad(id);
+                pad = gamepad.name().to_string();
                 
-                // check if it is a connection event
-                if event == gilrs::ev::EventType::Connected {
-                    let gamepad = gilrs.gamepad(id);
-                    
-                    println!("connected: {:?}; power: {:?}; ff: {:?}", gamepad.name(), gamepad.power_info(), gamepad.is_ff_supported());
-                }
+                println!("connected: {:?}; power: {:?}; ff: {:?}", pad, gamepad.power_info(), gamepad.is_ff_supported());
             }
+
+            let unix_time = time.duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis();
+            let rock = Rock {
+                at: unix_time,
+                pad: pad.clone(),
+                event,
+            };
+
+            let serialized = serde_json::to_string(&rock).unwrap();
+
+            println!("serialized = {}", serialized);
+            
+            // db.put(unix_time.to_ne_bytes(), serialized).unwrap();
         }
+    }
     });
 
     tauri::Builder::default()
