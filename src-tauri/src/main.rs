413cddf042596754707ee55d2817e568f2f9710a
JoeyShapiro <joeyashapiro@gmail.com>
2024-07-24 21:26:44 -0500

    added dummy data and past day

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{process::exit, time::{SystemTime, UNIX_EPOCH}};
+use std::{io::Read, process::exit, time::{SystemTime, UNIX_EPOCH}};
 use chrono::prelude::*;
 
 use rocksdb::{DB, Options};
@@ -42,9 +42,10 @@ struct Rock {
     event: gilrs::EventType,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct Point {
     data: u32,
+    at: u128, // used for sorting
     label: String,
 }
 
@@ -59,6 +60,7 @@ fn applications() -> Vec<Application> {
 
     let iter = db.iterator(rocksdb::IteratorMode::Start);
     for row in iter {
+        println!("row = {:?}", row);
         let (key, value) = row.unwrap();
         let value = String::from_utf8(value.into_vec()).unwrap();
 
@@ -81,27 +83,51 @@ fn applications() -> Vec<Application> {
 }
 
 fn past_day() -> Vec<Point> {
-    let mut points = Vec::new();
-
     let unix_time = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis();
     // get the time 24 hours ago
     let unix_time_24 = unix_time - 86_400_000;
 
-    for i in 0..24 {
-        let unix_day = unix_time_24 + (i as u128 * 3_600_000);
-        // format to 12 hour time
-        let time = SystemTime::UNIX_EPOCH + std::time::Duration::from_millis(unix_day as u64);
-        let datetime = DateTime::<Utc>::from(time);
-        // Formats the combined date and time with the specified format string.
-        let timestamp_str = datetime.format("%I %p").to_string();
+    // could do a vec, but would need one for each day anyway.
+    // this would be better for adding hours with no data
+    let mut buckets: [Point; 24] = std::array::from_fn(|i| Point { 
+        data: 0, label: String::new(), at: unix_time_24 + (i as u128 * 3_600_000)
+    });
 
-        points.push(Point {
-            data: i * 10,
-            label: timestamp_str,
-        });
+    // open an iter to read the db
+    let path = "coca-rocks.db";
+    let opts = Options::default();
+    let db = DB::open(&opts, path).unwrap();
+
+        // let mut ro = rocksdb::ReadOptions::default();
+        // // Set bounds to test that set_iterate_range clears old bounds.
+        // ro.set_iterate_lower_bound(vec![b'z']);
+        // ro.set_iterate_upper_bound(vec![b'z']);
+        // ro.set_iterate_range(range);
+
+    // let mut iter = .into_iter();
+    let mut iter = db.iterator(rocksdb::IteratorMode::Start);
+    for row in iter {
+        let (key, _value) = row.unwrap();
+        let at = u128::from_ne_bytes(key.into_vec().try_into().unwrap());
+        println!("at = {}", at);
+        // let at = SystemTime::UNIX_EPOCH + std::time::Duration::from_millis(test as u64);
+
+        // add the data to the proper bucket
+        for i in 0..24 {
+            // this will deal with oob
+            if at >= buckets[i].at && at < buckets[i].at + 3_600_000 {
+                buckets[i].data += 1;
+                break;
+            }
+        }
     }
 
-    points
+    // let time = SystemTime::UNIX_EPOCH + std::time::Duration::from_millis(unix_day as u64);
+    // let datetime = DateTime::<Utc>::from(time);
+    // // Formats the combined date and time with the specified format string.
+    // let timestamp_str = datetime.format("%I %p").to_string();
+
+    buckets.to_vec()
 }
 
 fn past_week() -> Vec<Point> {
@@ -122,6 +148,7 @@ fn past_week() -> Vec<Point> {
         points.push(Point {
             data: i * 10,
             label: timestamp_str,
+            at: unix_day,
         });
     }
 
@@ -176,6 +203,44 @@ fn main() {
     // }
     // let _ = DB::destroy(&Options::default(), path);
 
+    let path = "coca-rocks.db";
+    let mut opts = Options::default();
+    opts.create_if_missing(true);
+    // open default: 15.5MiB (111k)
+    let db = DB::open(&opts, path).unwrap();
+
+    // insert 1000 values of dummy data
+    let pad = "PS5 Controller".to_string();
+    let data = "{\"AxisChanged\":[\"LeftStickY\",0.010416665,{\"page\":1,\"usage\":49}]}";
+    let event = serde_json::from_str(data).unwrap();
+
+    let start = SystemTime::now();
+    let n = 100000;
+    let unix_time = start.duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis() - (n as u128) * 100;
+    for i in 0..n {
+        let app = match rand::random::<u32>() % 5 {
+            0 => "Skyrim".to_string(),
+            1 => "Minecraft".to_string(),
+            2 => "Hatsune Miku Project Diva 2nd Stage".to_string(),
+            3 => "Muse Dash".to_string(),
+            4 => "Tekken 8".to_string(),
+            _ => "?".to_string(),
+        };
+
+        let rock = Rock {
+            at: unix_time + (i as u128) * 100,
+            pad: pad.clone(),
+            app: app.clone(),
+            event,
+        };
+
+        let serialized = serde_json::to_string(&rock).unwrap();
+        db.put(unix_time.to_ne_bytes(), serialized).unwrap();
+    }
+    let end = SystemTime::now();
+    println!("inserted {n} values in {:?}", end.duration_since(start).unwrap());
+    exit(0);
+
     // run gilrs in a separate thread
     let gilrs_thread = std::thread::spawn(|| {
     let mut gilrs = Gilrs::new().unwrap();
@@ -184,38 +249,6 @@ fn main() {
     // opts.create_if_missing(true);
     // // open default: 15.5MiB (111k)
     // let db = DB::open(&opts, path).unwrap();
-
-    // // insert 1000 values of dummy data
-    // let pad = "PS5 Controller".to_string();
-    // let data = "{\"AxisChanged\":[\"LeftStickY\",0.010416665,{\"page\":1,\"usage\":49}]}";
-    // let event = serde_json::from_str(data).unwrap();
-
-    // let start = SystemTime::now();
-    // for _i in 0..100000 {
-    //     let app = match rand::random::<u32>() % 5 {
-    //         0 => "Skyrim".to_string(),
-    //         1 => "Minecraft".to_string(),
-    //         2 => "Hatsune Miku Project Diva 2nd Stage".to_string(),
-    //         3 => "Muse Dash".to_string(),
-    //         4 => "Tekken 8".to_string(),
-    //         _ => "?".to_string(),
-    //     };
-
-    //     let time = SystemTime::now();
-    //     let unix_time = time.duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis();
-    //     let rock = Rock {
-    //         at: unix_time,
-    //         pad: pad.clone(),
-    //         app: app.clone(),
-    //         event,
-    //     };
-
-    //     let serialized = serde_json::to_string(&rock).unwrap();
-    //     db.put(unix_time.to_ne_bytes(), serialized).unwrap();
-    // }
-    // let end = SystemTime::now();
-    // println!("inserted 1000 values in {:?}", end.duration_since(start).unwrap());
-    // exit(0);
 
     // Iterate over all connected gamepads
     for (_id, gamepad) in gilrs.gamepads() {
